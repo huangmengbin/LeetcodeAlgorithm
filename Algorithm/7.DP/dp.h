ecf5c4b3dec9d0cbc6d170fe0c412c0d1edbeca6
HMB <1352482583@qq.com>
2021-02-16 23:50:02 +0800

    feat: DP

--- Algorithm/7.DP/dp.h
+++ Algorithm/7.DP/dp.h
@@ -8,5 +8,72 @@
 class DP{
 public:
 
+
+    /**
+     * 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，
+     * 影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，
+     * 如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+     *
+     *     dp[i] = max(dp[i - 1], nums[i] + dp[i - 2]);
+     *
+     *     为什么这个是对的，确实不那么直观... 还是得仔细思索
+     */
+
+
+    int rob1(const vector<int>& nums) {
+        if(nums.empty()){
+            return 0;
+        }
+        int *dp = new int[nums.size()];
+        for(int i = 0; i<nums.size(); i++){
+            if(i==0){
+                dp[0]=nums[0];
+            } else if(i==1){
+                dp[1] = max(dp[0], nums[1]);
+            } else{
+                dp[i] = max(dp[i - 1], nums[i] + dp[i - 2]);
+            }
+        }
+        return dp[nums.size() - 1];
+    }
+
+
+    int rob2(vector<int>& nums) {
+        if(nums.empty()){
+            return 0;
+        }
+        if(nums.size() == 1){
+            return nums.at(0);
+        }
+        int end_num = nums.back();
+        nums.pop_back();
+        int res1 = rob1(nums);
+        nums.push_back(end_num);
+        nums.erase(nums.begin());
+        return max(res1, rob1(nums));
+    }
+
+    int minPathSum(vector<vector<int>>& grid) {
+        int row = grid.size(), column = grid[0].size();
+        assert(row * column);
+        int **const dp = new int *[row];
+        for(int i = 0;i<row; i++){
+            dp[i] = new int [column];
+        }
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < row; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int j = 1; j < column; j++) {
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < column; j++) {
+                dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[row - 1][column - 1];
+    }
+
 };
 #endif //LEETCODE_DP_H
